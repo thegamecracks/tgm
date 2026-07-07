e07dd755dc3dfd91f9fca5ac23a04c4d63e82cb9
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-07-07 08:19:16 -0400

    test: cover a few more fix-meta cases

--- tests/test_fix_meta.py
+++ tests/test_fix_meta.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import re
 from typing import TYPE_CHECKING
 
+from pytest import LogCaptureFixture
+
 from tgm import Config, FixMeta
 
 if TYPE_CHECKING:
@@ -29,3 +31,30 @@ def test_fix_meta_dry_run(config: Config, workshop: Workshop) -> None:
     m = re.search(r"publishedid = (\d+);", meta)
     assert m is not None, "missing publishedid in meta.cpp"
     assert m[1] == "0", "violated dry run"
+
+
+def test_fix_meta_file_not_found(
+    config: Config,
+    workshop: Workshop,
+    caplog: LogCaptureFixture,
+) -> None:
+    item_id = 1234567890
+    workshop_path = workshop.install_workshop_item(item_id)
+    workshop_path.joinpath("meta.cpp").unlink()
+    FixMeta(config, dry_run=False).invoke()
+
+    assert caplog.records[0].getMessage() == f"Missing meta.cpp in {item_id}"
+
+
+def test_fix_meta_add_publishedid(
+    config: Config,
+    workshop: Workshop,
+) -> None:
+    item_id = 1234567890
+    content = "foo = 123;\n"
+    workshop_path = workshop.install_workshop_item(item_id)
+    workshop_path.joinpath("meta.cpp").write_text(content)
+    FixMeta(config, dry_run=False).invoke()
+
+    meta = workshop_path.joinpath("meta.cpp").read_text()
+    assert meta == f"{content}publishedid = {item_id};\n"
